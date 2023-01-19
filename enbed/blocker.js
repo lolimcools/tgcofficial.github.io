21bf44c7827ad6623f5751d00fcf20827a9b0105
tgcofficial <123127031+tgcofficial@users.noreply.github.com>
2023-01-19 17:58:01 -0600

    Create blocker.js

---
+++ enbed/blocker.js
@@ -0,0 +1,10 @@
+if(window.self != window.top) {
+try {
+if(window.parent.location.hostname.indexOf("https://tgcofficial.github.io") == -1) {
+window.location.href = "https://tgcofficial.github.io";
+} else {
+}
+} catch (ex) {
+window.location.href = "https://tgcofficial.github.io/enbed/blocker.index.html";
+}
+} 
