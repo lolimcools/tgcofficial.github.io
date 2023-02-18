7e1e01ea609dcdc147c1c581ad2855ef4d5a8679
tgcofficial <123127031+tgcofficial@users.noreply.github.com>
2023-02-18 17:19:08 -0600

    Update blocker.js

--- enbed/blocker.js
+++ enbed/blocker.js
@@ -1,10 +1,30 @@
-if(window.self != window.top) {
-try {
-if(window.parent.location.hostname.indexOf("https://tgcofficial.github.io") == -1) {
-window.location.href = "https://tgcofficial.github.io";
-} else {
+// unframed 
+var blocked = `
+    <link href="/css/enbed.css" rel="stylesheet" type="text/css"/>
+
+  <style>
+  
+    body {
+      transform: translate(-50%, -50%);
+      text-align: center;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+    }
+      
+    h1 { font-size: 25px;}
+    .btn { width: 75%; font-weight: bold; font-size: 13px; margin: 5px;}
+  
+  </style>
+  <h1>This embed was blocked by Unframed</h1> <br>
+  <button class="btn btn-secondary" onclick="UnframedRedirect()">Go Back</button>
+`
+if (window.self != window.top) {
+  if (!document.referrer.includes(self.location)) {
+    document.querySelector("html").innerHTML = blocked
+  }
 }
-} catch (ex) {
-window.location.href = "https://tgcofficial.github.io/enbed/blocker.index.html";
+
+function UnframedRedirect() {
+  open(self.location)
 }
-} 
