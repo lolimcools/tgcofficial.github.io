287c37c384d6018d01a46988bc5140a42b54b21a
tgcofficial <123127031+tgcofficial@users.noreply.github.com>
2023-02-19 00:03:55 -0600

    Update subtitles.js

--- js/subtitles.js
+++ js/subtitles.js
@@ -66,7 +66,7 @@ say[58] = "v.5 is out!";
 say[59] = "If you microwave your phone it will become faster!";
 say[60] = "Welcome to my rad website";
 say[61] = "coems 🤑";
-say[62] = "Ok, and I know your address now!"
+say[62] = "Ok, and I know your address now!";
 
 
 
